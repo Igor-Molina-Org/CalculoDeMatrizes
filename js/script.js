54283b69ee8c2982b95a6eb39d31346d636dc82a
Igor Molina <igoramil452@gmai.com>
2025-04-23 19:52:20 -0300

    feat: added customized alert with initial styles. Removed getOperator, achieved same functionality through default statement in switch case

--- js/script.js
+++ js/script.js
@@ -1,5 +1,4 @@
 import * as DomHelper from "./Helpers/domHelper.js";
-import * as MatrixHelper from "./Helpers/matrixHelper.js";
 
 function validateInput(number) {
   number.value = number.value.replace(/[^0-9]/g, "").slice(0, 1); // mantém apenas um número
